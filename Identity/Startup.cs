78d1ab04c177a0ab8b01e784afa1dfdbab91ddf8
tlhbaris <talhabaris97@gmail.com>
2022-11-19 17:15:49 +0300

    User validation

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -34,6 +34,15 @@ namespace Identity
 
             services.AddIdentity<AppUser, AppRole>(opts =>
             {
+
+
+                opts.User.RequireUniqueEmail = true;
+                opts.User.AllowedUserNameCharacters = "abcçdefghýijklmnoöpqrsþtuüvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+
+
+
+
+
                 opts.Password.RequiredLength = 4;
                 opts.Password.RequireNonAlphanumeric = false;
                 opts.Password.RequireLowercase= false;              // þifre doðrulama ayarlarý
